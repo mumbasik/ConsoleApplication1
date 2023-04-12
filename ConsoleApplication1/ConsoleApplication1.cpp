7696b2b7b3ddf902c310fd49125b99daf8a418a0
zadrot <zadrot@DESKTOP-L5ER55A>
2023-04-12 19:58:26 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,16 @@
+﻿
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "I love C++!\n";
+    std::cout << "\t I love C++!\n";
+    std::cout << "\t \t I love C++!\n";
+    std::cout << "\t \t \t I love C++!\n";
+    std::cout << "\t \t \t \t I love C++!\n";
+
+
+
+}
+
